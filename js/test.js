f4673954249ffa63150f12417f9f790e07008f75
Jasmine Chan <chan.jasmine4@gmail.com>
2020-01-18 19:16:58 +0800

    Added about.html

--- js/test.js
+++ js/test.js
@@ -1,7 +1,5 @@
 let btn1 = document.getElementById('btn1');
-// btn1.innerHTML = "Hello";
-let count = 0;
 
 btn1.addEventListener('click', () => {
-    btn1.innerHTML = count++;
+    location.href = './about.html';
 });
